222f4d976058acc2bf610b1718bfa4680ae1b610
Erik Benoist <ebenoist@gmail.com>
2016-05-14 09:49:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+protoc-gen-doc Homebrew Formula
+---
+
+```BASH
+brew tap ebenoist/protoc-gen-doc
+brew install protoc-gen-doc
+```
---
+++ protoc-gen-doc.rb
@@ -0,0 +1,22 @@
+class ProtocGenDoc < Formula
+  desc "Documentation generator plugin for Google Protocol Buffers"
+  homepage "https://github.com/estan/protoc-gen-doc"
+  url "https://github.com/estan/protoc-gen-doc/archive/v0.8.tar.gz"
+  version "0.8"
+  sha256 "fabd54c9bc75558a659ee0f1e52fc2c65014084de4aa5e329a3778c2c88155ee"
+  head "https://github.com/estan/protoc-gen-doc.git"
+
+  depends_on "qt5"
+
+  def install
+    protobuf_prefix = `#{HOMEBREW_PREFIX}/bin/brew --prefix protobuf`.chomp!
+    ENV["PROTOBUF_PREFIX"] = protobuf_prefix
+    system "qmake"
+    system "make"
+    bin.install "protoc-gen-doc"
+  end
+
+  test do
+    system "echo test > proto-gen-doc"
+  end
+end
